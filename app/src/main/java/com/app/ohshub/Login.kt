cbd7975d16a956f0c627c0dbe5031ff2c17af469
Amirul Faqih <fakiiahmad001@gmail.com>
2024-09-03 19:58:34 +0300

    first commit

---
+++ app/src/main/java/com/app/ohshub/Login.kt
@@ -0,0 +1,42 @@
+package com.app.ohshub
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val usernameEditText = findViewById<EditText>(R.id.etUsername)
+        val passwordEditText = findViewById<EditText>(R.id.etPassword)
+        val loginButton = findViewById<Button>(R.id.btnLogin)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            // Perform login logic here
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter your credentials", Toast.LENGTH_SHORT).show()
+            } else {
+//                val username = "JohnDoe"
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("USERNAME", username)
+                startActivity(intent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/app/ohshub/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.app.ohshub
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Get the Intent that started this activity and extract the string
+        val username = intent.getStringExtra("USERNAME")
+
+        // Use the received data (e.g., display it in a TextView)
+//        val textView = findViewById<TextView>(R.id.textView)
+//        textView.text = "Welcome, $username!"
+    }
+}
